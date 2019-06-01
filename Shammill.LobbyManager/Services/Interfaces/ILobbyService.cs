edac9b1c43ea544c55e6ef9fa7f6669d0a6e4440
shammill <shammill88@gmail.com>
2019-06-01 11:21:21 +1000

    LobbyService, and StatusController

---
+++ Shammill.LobbyManager/Services/Interfaces/ILobbyService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Shammill.LobbyManager.Models;
+using Shammill.LobbyManager.Models.Requests;
+
+namespace Shammill.LobbyManager.Services.Interfaces
+{
+    public interface ILobbyService
+    {
+        Lobby CreateLobby(CreateLobbyRequest lobbyRequest);
+        void DestroyLobby(Guid lobby);
+
+        Lobby UpdateLobbyDetails(Lobby lobby);
+
+        bool AddPlayerToLobby(Guid lobby, Player playerId);
+        bool RemovePlayerFromLobby(Guid lobby, Player playerId);
+        bool ChangeLobbyLeader(Guid lobby, Player playerId);
+
+        List<Lobby> GetLobbies(LobbyFilter lobbyFilter);
+    }
+}
---
+++ Shammill.LobbyManager/Services/LobbyService.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shammill.LobbyManager.Models;
+using Shammill.LobbyManager.Models.Requests;
+using Shammill.LobbyManager.Persistance;
+using Shammill.LobbyManager.Services.Interfaces;
+
+namespace Shammill.LobbyManager.Services
+{
+    public class LobbyService: ILobbyService
+    {
+        // Will move away from a singleton eventually, HA'd etc.
+        Dictionary<Guid, Lobby> lobbies = LobbyCache.Instance.Lobbies;
+
+        public LobbyService()
+        {
+            
+        }
+
+        public Lobby CreateLobby(CreateLobbyRequest createLobbyRequest)
+        {
+            //maybe use automapper
+            Lobby lobby = new Lobby()
+            {
+                Id = Guid.NewGuid(),
+                Name = createLobbyRequest.LobbyName,
+                IsPublic = createLobbyRequest.IsPublic,
+                Region = createLobbyRequest.Region,
+                MaximumSize = createLobbyRequest.MaximumSize,
+                GameMode = createLobbyRequest.GameMode,
+                IsJoinable = createLobbyRequest.IsJoinable,
+                Players = new List<Player>()
+            };
+            // Add lead player
+            lobbies.Add(lobby.Id, lobby);
+
+            return lobby;
+        }
+
+        public void DestroyLobby(Guid lobbyId) {
+            lobbies.Remove(lobbyId);
+        }
+
+        public Lobby UpdateLobbyDetails(Lobby lobby) {
+            lobbies[lobby.Id] = lobby;
+            return lobby;
+        }
+
+        // player based
+        public bool AddPlayerToLobby(Guid lobbyId, Player player) {
+            lobbies[lobbyId].Players.Add(player);
+            return true;
+        }
+
+        public bool RemovePlayerFromLobby(Guid lobbyId, Player player) {
+            lobbies[lobbyId].Players.Remove(player);
+            return true;
+        }
+
+        public bool ChangeLobbyLeader(Guid lobbyId, Player playerLeader) {
+            var isSuccessful = false;
+            foreach (var player in lobbies[lobbyId].Players)
+            {
+                if (player.Id == playerLeader.Id)
+                {
+                    player.IsLobbyLeader = true;
+                    isSuccessful = true;
+                }
+                else
+                {
+                    player.IsLobbyLeader = false;
+                }
+            }
+            return isSuccessful;
+        }
+
+        // searching/getting
+        public List<Lobby> GetLobbies(LobbyFilter lobbyFilter)
+        {
+            var filteredLobbies = lobbies.Where(lobby => lobby.Value.Region == lobbyFilter.Region)
+                    .Where(lobby => lobby.Value.IsPublic == true)
+                    .Where(lobby => lobby.Value.HasGameInProgress == lobbyFilter.HasGameInProgress)
+                    .AsQueryable();
+
+            if (lobbyFilter.IsNotFull)
+            {
+                filteredLobbies = filteredLobbies.Where(x => x.Value.Players.Count < x.Value.MaximumSize);
+            }
+
+            return filteredLobbies
+                    .Select(x => x.Value)
+                    .ToList();
+        }
+    }
+}
