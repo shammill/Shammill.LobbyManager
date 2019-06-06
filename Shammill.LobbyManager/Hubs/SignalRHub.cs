c64f557b07f1c331aea131bd3c8c6a4e945030fa
shammill <shammill88@gmail.com>
2019-06-06 21:28:11 +1000

    upgraded packages to latest with VS2019 and added signalr hub which we will use to update players when changes occur

---
+++ Shammill.LobbyManager/Hubs/SignalRHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Shammill.LobbyManager.Hubs
+{
+    public class SignalRHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.User("").SendAsync(""); //.SendMessage("test");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
